4355178c3bd53548528d92e2e767b4e883754ed8
Daniel Veloso <daniel.f.veloso@ufv.br>
2025-11-22 14:57:44 -0300

    replace True for False in output_dir

--- model_training/rnn_trainer.py
+++ model_training/rnn_trainer.py
@@ -57,11 +57,11 @@ class BrainToTextDecoder_Trainer:
 
         # Create output directory
         if args['mode'] == 'train':
-            os.makedirs(self.args['output_dir'], exist_ok=False)
+            os.makedirs(self.args['output_dir'], exist_ok=True)
 
         # Create checkpoint directory
         if args['save_best_checkpoint'] or args['save_all_val_steps'] or args['save_final_model']: 
-            os.makedirs(self.args['checkpoint_dir'], exist_ok=False)
+            os.makedirs(self.args['checkpoint_dir'], exist_ok=True)
 
         # Set up logging
         self.logger = logging.getLogger(__name__)
